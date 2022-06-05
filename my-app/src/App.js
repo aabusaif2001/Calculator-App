6b58572a5278af44bf472aa1912f7f0b145b6f9d
Ayman Abusaif <aabusaif2001@gmail.com>
2022-06-05 05:31:06 -0500

    forms and labels

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,25 @@ function App() {
     <div className="App">
       <h1>Financial Independence Calculator</h1>
       <h2>You can retire at age</h2>
+      <div>Target Retirement Amount: </div>
+      <form>
+        <label>
+          Annual Retirement Expenses (today's dollars)
+          <input type="number"/>
+        </label>
+        <label>
+         Current Age           
+        <input type="number"/>
+        </label>
+        <label>
+          Current Savings Balance
+          <input type="number"/>
+        </label>
+        <label>
+          Regular Contributions
+          <input type="number"/>
+        </label>
+      </form>
     </div>
   );
 }
