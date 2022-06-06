41a9ea35a0ff0b5af13f3755f8114557a339b1a5
Ayman Abusaif <aabusaif2001@gmail.com>
2022-06-06 05:02:37 -0500

    key values made to handle information

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const initialRetirementAge = Number(localStorage.getItem("retirementAge") || 100);
+  const initialTargetRetAmt = Number(localStorage.getItem("targetRetAmt") || 0);
+  const initialAnnualRetExp = Number(localStorage.getItem("annualRetExp") || 0);
+  const initialCurrentAge = Number(localStorage.getItem("currentAge") || 35);
+  const initialCurrentSavings = Number(localStorage.getItem("currentSavings") || 10000);
+  const initialContributions = Number(localStorage.getItem("contributions") || 500);
+  const initialContributionFreq = Number(localStorage.getItem("contributionFreq") || "Monthly");
+  const initialPreRetROR = Number(localStorage.getItem("preRetROR") || 7);
+  const initialPostRetROR = Number(localStorage.getItem("postRetROR") || 7);
+  const initialInflation = Number(localStorage.getItem("inflation") || 2.9);
   return (
     <div className="App">
       <h1>Financial Independence Calculator</h1>
       <h2>You can retire at age</h2>
       <div>Target Retirement Amount: </div>
-      <form>
+      <form className='fire-calc-form'>
         <label>
           Annual Retirement Expenses (today's dollars)
           <input type="number"/>
